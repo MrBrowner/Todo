bcf35ec2ab0d03ff6a23df2cb462215151c67c24
browne_as <24374030+MrBrowner@users.noreply.github.com>
2024-03-09 21:57:16 +0530

    ** koin: module;

--- app/src/main/java/com/example/todo/di/KoinModule.kt
+++ app/src/main/java/com/example/todo/di/KoinModule.kt
@@ -1,14 +1,37 @@
 package com.example.todo.di
 
+import com.example.todo.dataOBox.network.model.AnimeQuoteService
+import com.example.todo.dataOBox.repo.AnimeQuoteRepoImpl
 import com.example.todo.dataRealm.model.Todo
+import com.example.todo.domain.repo.AnimeQuoteRepo
+import com.example.todo.vm.AnimeQuoteVM
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val koinModule = module {
     single { Realm.open(RealmConfiguration.create(schema = setOf(Todo::class))) }
 
-//    viewModel { MainVM() }
-//    scope<MainActivity> {
-//    }
+    single<Json> {
+        Json { ignoreUnknownKeys = true }
+    }
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl("https://animechan.xyz/api")
+            .addConverterFactory(get<Json>().asConverterFactory("application/json".toMediaType())) // should add it at last
+            .build()
+    }
+
+    single<AnimeQuoteService> {
+        get<Retrofit>().create(AnimeQuoteService::class.java)
+    }
+
+    single<AnimeQuoteRepo> { AnimeQuoteRepoImpl(get<AnimeQuoteService>()) }
+    viewModelOf(::AnimeQuoteVM)
 }
