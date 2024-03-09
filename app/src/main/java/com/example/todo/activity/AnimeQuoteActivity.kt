661363bdf932883ca2db947c0a0b5a984582f875
browne_as <24374030+MrBrowner@users.noreply.github.com>
2024-03-09 23:09:44 +0530

    ** liveData, stateFlow;

--- app/src/main/java/com/example/todo/activity/AnimeQuoteActivity.kt
+++ app/src/main/java/com/example/todo/activity/AnimeQuoteActivity.kt
@@ -2,8 +2,13 @@ package com.example.todo.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.todo.databinding.ActivityAnimeQuoteBinding
 import com.example.todo.vm.AnimeQuoteVM
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -20,6 +25,15 @@ class AnimeQuoteActivity : AppCompatActivity() {
         Timber.e(vm.msg)
         vm.quote.observe(this) {
             Timber.tag("quote").e(it.toString())
+        }
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                vm.q2.collectLatest {
+                    Timber.tag("q2").e(it.toString()) // initial data (null) is also emitted
+                }
+            }
         }
 
         bind.btnShuffle.setOnClickListener {
--- app/src/main/java/com/example/todo/vm/AnimeQuoteVM.kt
+++ app/src/main/java/com/example/todo/vm/AnimeQuoteVM.kt
@@ -2,10 +2,12 @@ package com.example.todo.vm
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.todo.domain.model.AnimeQuote
 import com.example.todo.domain.repo.AnimeQuoteRepo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -15,15 +17,18 @@ class AnimeQuoteVM(
 
     val msg = "Hey"
     val quote = MutableLiveData<AnimeQuote?>()
+    val q2 = MutableStateFlow<AnimeQuote?>(null)
 
     fun generateRandomQuote() {
         viewModelScope.launch(Dispatchers.IO) {
             quoteRepo.getRandomQuote()
                 .onSuccess {
                     quote.postValue(it)
+                    q2.emit(it)
                 }.onFailure {
                     Timber.e(it)
                     quote.postValue(null)
+                    q2.emit(null)
                 }
         }
     }
